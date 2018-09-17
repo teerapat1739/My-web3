db778e435d35b5219db6654bf4223f843076dc90
Teerapat Tee <teerapat12345678@gmail.com>
2018-09-17 13:44:00 +0700

    first trantraction

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDwalletProvider = require('truffle-hdwallet-provider')
+const Web3 = require('web3')
+const { interface, bytecode } = require('./complie')
+
+const provider = new HDwalletProvider(
+    'era talk kitten taste summer actor grant verify hotel elder wrestle avoid',
+    'https://rinkeby.infura.io/v3/3c1cacfb1a124f99901b55f98c7e4d27'
+);
+
+const web3 = new Web3(provider)
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from accont', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: '0x' + bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '2000000' });
+
+    console.log('Contract to', result.options.address);
+
+};
+
+deploy();
