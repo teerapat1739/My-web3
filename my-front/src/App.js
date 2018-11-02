3b4a980ddb7ae4c6178e1af870123cfbc19e8327
Teerapat Tee <teerapat12345678@gmail.com>
2018-11-02 16:15:39 +0700

    ui form setup

--- my-front/src/App.js
+++ my-front/src/App.js
@@ -2,26 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
+import lottery from "./lottery";
 
 class App extends Component {
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: ''
+  }
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call()
+    const players = await lottery.methods.getPlayers().call()
+    const balance = await web3.eth.getBalance(lottery.options.address)
+
+    this.setState({ manager, players, balance })
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault()
+
+    const accounts = await web3.eth.getAccounts()
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <h2>Lottery Contract</h2>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            This contract is magaged by {this.state.manager}.
+            There are currently {this.state.players.length} people entered,
+            competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-           {web3.version}
-          </a>
-        </header>
-      </div>
+
+          <hr />
+
+          <form onSubmit={this.onSubmit}>
+            <h4>Want to try your luck?</h4>
+            <div>
+              <label>Amount of ether to enter</label>
+              <input
+                value={this.state.value}
+                onChange={event => this.setState({ value: event.target.value})}
+              />
+            </div>
+            <button>Enter</button>
+          </form>
+        </div>
     );
   }
 }
